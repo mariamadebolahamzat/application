4683562df8260b5a7f8471485c121ed0dffb572e
Ifihan <victoriaolusheye@gmail.com>
2022-10-31 23:28:22 +0100

    Added commented code

--- codes.py
+++ codes.py
@@ -1,3 +1,11 @@
+# Program to generate a random number between 0 and 9
+
+# importing the random module
+# import random
+
+# print(random.randint(0,9))
+
+
 from pytube import Playlist
 p = Playlist('playlist link here')
 
